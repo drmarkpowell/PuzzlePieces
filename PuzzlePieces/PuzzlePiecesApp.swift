5d2f04031a20686dad3c0c1a66c50a9d49e99beb
drmarkpowell <Mark.Powell@computer.org>
2024-09-29 15:35:29 -0700

    Initial Commit

---
+++ PuzzlePieces/PuzzlePiecesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzlePiecesApp.swift
+//  PuzzlePieces
+//
+//  Created by Mark Powell on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PuzzlePiecesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
